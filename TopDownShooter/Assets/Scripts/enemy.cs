90c5b32de3add83f7e94c298b4075f948e5db043
lennert93 <lennert.hagmann@tu-dortmund.de>
2023-04-15 14:32:21 +0200

    Enemy bewegt sich auf Player zu, Enemy_weak erstellt

--- TopDownShooter/Assets/Scripts/enemy.cs
+++ TopDownShooter/Assets/Scripts/enemy.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class enemy : MonoBehaviour
@@ -13,11 +15,14 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //added
+        lookingForPlayer();
 
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "bullet")
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bullet")
         {
             Debug.Log("test");
             bulletinfo info = other.GetComponent<bulletinfo>();
@@ -32,9 +37,57 @@ public class enemy : MonoBehaviour
     {
         health -= amount;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             Destroy(this.gameObject);
         }
+    }
+
+    
+    /* 15.04.: Idee:
+     * Enemy schaut sich um (links oder rechts um seine eigene Achse).
+     * Sobald er den Player sieht, läuft er auf ihn zu.
+     * Hinzulaufen ist in moving() realisiert, weiß aber nicht ob es eine effizientere Möglichkeit gibt
+     * 
+    */
+    
+    public float enemySpeed;
+    private void moving()
+    {
+        this.GetComponent<Rigidbody>().transform.position += getVectorToPlayerPosition() * enemySpeed * Time.fixedDeltaTime;
+
+    }
+
+    //berechnet Position Enemy - Position Spieler und gibt den umgekehrten Vector zurück
+    private Vector3 getVectorToPlayerPosition()
+    {
+        Vector3 enemyPosition = this.GetComponent<Rigidbody>().transform.position;
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position; //changed Tag from player to "Player"
+        // Debug.Log(playerPosition);
+        Vector3 x = new Vector3(
+            enemyPosition.x - playerPosition.x,
+            0,
+            enemyPosition.z - playerPosition.z);
+        return -x;
+    }
+
+    private Vector3 RotationVector = new Vector3(0f, 1f, 0f);
+    private float rotationSpeed = 0.2f;
+
+    //umschauen, bei Sichtkontakt moving
+    private void lookingForPlayer()
+    {
+        this.GetComponent<Rigidbody>().transform.Rotate(RotationVector * rotationSpeed, Space.World);
+        if (seePlayer())
+        {
+            moving();
+            //facing
+        }
+    }
+
+    //
+    private Boolean seePlayer()
+    {
+        return true;
     }
 }
---
+++ TopDownShooter/Assets/Scripts/enemy_weak.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+
+public class enemy_weak : enemy
+{
+
+
+}
